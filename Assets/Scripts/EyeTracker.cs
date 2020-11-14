b5844bcac34c0193cf80f6a5d4e1affd9742c165
Jackson Barnes <atx.barnes@gmail.com>
2020-11-13 18:16:12 -0600

    Got the sim working

--- Assets/Scripts/EyeTracker.cs
+++ Assets/Scripts/EyeTracker.cs
@@ -3,19 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using TMPro;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(ARFace))]
 public class EyeTracker : MonoBehaviour
 {
     [SerializeField]
     private GameObject eyePrefab;
+
+    [SerializeField]
+    private GameObject reticlePrefab;
+
+    private TextMeshProUGUI rightEyeTelemetryTMP;
+    private TextMeshProUGUI leftEyeTelemetryTMP;
+
     private GameObject eyeLeft;
     private GameObject eyeRight;
     private ARFace arFace;
 
+    private Canvas canvas;
+
+    Image imgReticle;
+
     void Awake()
     {
         arFace = this.GetComponent<ARFace>();
+        canvas = FindObjectOfType<Canvas>();
+
+        rightEyeTelemetryTMP = GameObject.FindGameObjectWithTag("Right Eye").GetComponent<TextMeshProUGUI>();
+        leftEyeTelemetryTMP = GameObject.FindGameObjectWithTag("Left Eye").GetComponent<TextMeshProUGUI>();
     }
 
     private void OnEnable()
@@ -43,6 +60,10 @@ public class EyeTracker : MonoBehaviour
         if(arFace.leftEye != null && eyeLeft == null)
         {
             eyeLeft = Instantiate(eyePrefab, arFace.leftEye);
+
+            // Move this
+            imgReticle = Instantiate(reticlePrefab, canvas.transform).GetComponent<Image>();
+
             eyeLeft.SetActive(false);
         }
 
@@ -55,6 +76,21 @@ public class EyeTracker : MonoBehaviour
         bool arState = (arFace.trackingState == TrackingState.Tracking) && (ARSession.state > ARSessionState.Ready);
 
         SetVisibility(arState);
+
+        Vector3 eulerAnglesLeftEye = arFace.leftEye.localEulerAngles;
+        Vector3 eulerAnglesRightEye = arFace.rightEye.localEulerAngles;
+
+        rightEyeTelemetryTMP.text = "angles x: " + eulerAnglesRightEye.x + " y: " + eulerAnglesRightEye.y + " z: " + eulerAnglesRightEye.z;
+        leftEyeTelemetryTMP.text = "angles x: " + eulerAnglesLeftEye.x + " y: " + eulerAnglesLeftEye.y + " z: " + eulerAnglesLeftEye.z;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(eyeRight.transform.position, eyeRight.transform.TransformDirection(Vector3.left), out hit, Mathf.Infinity))
+        {
+            Debug.LogFormat("Right Eye Hit: " + hit.transform.name);
+        }
+
+        imgReticle.transform.position = FindObjectOfType<ARCameraManager>().GetComponent<Camera>().WorldToScreenPoint(arFace.rightEye.position);
     }
 
     private void SetVisibility(bool isVisible)
---
+++ Assets/Scripts/EyeTrackerSim.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EyeTrackerSim : MonoBehaviour
+{
+    public Transform LookAtPositon;
+
+    public GameObject RightEye;
+    public GameObject LeftEye;
+
+    public Image RightEyeScreenDot;
+    public Image LeftEyeScreenDot;
+
+    public Image ScreenReticle;
+
+    public GameObject PointRight;
+    public GameObject PointLeft;
+
+    public GameObject SpaceReticle;
+
+    public bool StopEyeGaze = false;
+
+    Plane PlaneNearCamera;
+
+    void Update()
+    {
+        PlaneNearCamera = new Plane(Camera.main.transform.forward, new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z + Camera.main.nearClipPlane));
+
+        if (!StopEyeGaze)
+        {
+            // Simulate looking directly at the device
+            RightEye.transform.LookAt(LookAtPositon, Vector3.up);
+            LeftEye.transform.LookAt(LookAtPositon, Vector3.up);
+        }
+
+        //Create a ray from the Mouse click position
+        Ray rightEyeRay = new Ray(RightEye.transform.position, LookAtPositon.position - RightEye.transform.position);
+        Ray leftEyeRay = new Ray(LeftEye.transform.position, LookAtPositon.position - LeftEye.transform.position);
+
+        float rightEnter = 0.0f;
+
+        Vector3 rightHitPoint = new Vector3();
+
+        if (PlaneNearCamera.Raycast(rightEyeRay, out rightEnter))
+        {
+            rightHitPoint = rightEyeRay.GetPoint(rightEnter);
+
+            PointRight.transform.position = rightHitPoint;
+
+            RightEyeScreenDot.transform.position = Camera.main.WorldToScreenPoint(rightHitPoint);
+        }
+
+        float leftEnter = 0.0f;
+
+        Vector3 leftHitPoint = new Vector3();
+
+        if (PlaneNearCamera.Raycast(leftEyeRay, out leftEnter))
+        {
+            leftHitPoint = leftEyeRay.GetPoint(leftEnter);
+
+            PointLeft.transform.position = leftHitPoint;
+
+            LeftEyeScreenDot.transform.position = Camera.main.WorldToScreenPoint(leftHitPoint);
+        }
+
+        Vector3 ScreenReticlePos = new Vector3((leftHitPoint.x + rightHitPoint.x) / 2, (leftHitPoint.y + rightHitPoint.y) / 2, Camera.main.transform.position.z - Camera.main.farClipPlane);
+        Vector3 WorldReticlePos = new Vector3((leftHitPoint.x + rightHitPoint.x) / 2, (leftHitPoint.y + rightHitPoint.y) / 2, Camera.main.transform.position.z + Camera.main.nearClipPlane);
+
+        SpaceReticle.transform.position = WorldReticlePos;
+
+        ScreenReticle.transform.position = Camera.main.WorldToScreenPoint(ScreenReticlePos);
+
+        Debug.DrawRay(RightEye.transform.position, LookAtPositon.position - RightEye.transform.position, Color.green);
+        Debug.DrawRay(LeftEye.transform.position, LookAtPositon.position - LeftEye.transform.position, Color.green);
+
+        Debug.DrawRay(RightEye.transform.position, RightEye.transform.position - new Vector3(RightEye.transform.position.x, RightEye.transform.position.y, Camera.main.farClipPlane - Camera.main.transform.position.z), Color.red);
+        Debug.DrawRay(LeftEye.transform.position, LeftEye.transform.position - new Vector3(LeftEye.transform.position.x, LeftEye.transform.position.y, Camera.main.farClipPlane - Camera.main.transform.position.z), Color.red);
+    }
+}
