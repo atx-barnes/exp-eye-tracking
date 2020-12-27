edd4120c9153de614e1dbea891c8236a49c3e334
Jackson Barnes <atx.barnes@gmail.com>
2020-12-27 15:53:36 -0600

    added ar scenes and prototype Roll-A-Ball game test bed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject Ball;
+
+    public float ForceMultiplier = 1;
+
+    void Update() {
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            RaycastHit hit;
+
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit)) {
+
+                if (hit.rigidbody != null) {
+
+                    Vector3 dir = (hit.point - Ball.transform.position).normalized;
+
+                    Ball.GetComponent<Rigidbody>().AddForceAtPosition(dir * ForceMultiplier, hit.point);
+                }
+            }
+        }
+    }
+}
